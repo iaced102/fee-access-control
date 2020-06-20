8853daa0445adaf2bb268fc79dd4adbeb1317b5d
vdbkpro <vdbkpro@gmail.com>
2020-06-20 17:43:48 +0700

    update messagebus library

--- Library/MessageBus.php
+++ Library/MessageBus.php
@@ -73,11 +73,57 @@ class MessageBus{
             } else {
                 $payload = json_decode($msg->payload,true);
                 if(is_callable($callback)){
-                    $callback($topic,$payload);
+                    $callback($msg->topic_name,$payload);
+                }
+            }
+            exit;
+        }
+    }
+    /**
+     * Dev create: Dinhnv
+     * CreateTime: 20/06/2020 
+     * Hàm subscribe đồng thời nhiều topic và call về hàm callback
+     * @param $topics : List các topic cần subscribe từ message bus. Kiểu Array [String]
+     * @param $callback : hàm callback để xử lý data lấy được
+     * @param $consumerId : consumerId: String | false -nếu là không xác định và sẽ lấy từ đầu đến hiện tại (không thể resume), ngược lại, nếu set consumerid thì có thể resume khi bắt đầu lại
+     * @return void
+     */
+    public static function subscribeMultiTopic($topics,$consumerId,$callback){
+        $conf = self::getKafkaConfig();
+        $offsetType = RD_KAFKA_OFFSET_BEGINNING;
+        $topicConf = new RdKafka\TopicConf();
+        if($consumerId!=false){
+            $conf->set('group.id', $consumerId);
+            $offsetType = RD_KAFKA_OFFSET_STORED;
+            
+            $topicConf->set('auto.commit.interval.ms', 100);
+            $topicConf->set('offset.store.method', 'broker');
+            $topicConf->set('auto.offset.reset', 'smallest');;
+        }
+
+        $rk = new RdKafka\Consumer($conf);
+        //
+        $queue = $rk->newQueue();
+        foreach($topics as $topic){
+            $topicObject = $rk->newTopic($topic,$topicConf);
+            $topicObject->consumeQueueStart(0, $offsetType,$queue);
+        }
+        while (true) {
+            $msg = $queue->consume(120000);
+            if (null === $msg || $msg->err === RD_KAFKA_RESP_ERR__PARTITION_EOF) {
+                continue;
+            } elseif ($msg->err) {
+                break;
+            } else {
+                $payload = json_decode($msg->payload,true);
+                
+                if(is_callable($callback)){
+                    $callback($msg->topic_name,$payload);
                 }
             }
         }
     }
+    
     /**
      * Dev create: Dinhnv
      * CreateTime: 18/06/2020 
