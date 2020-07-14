7d5602b40dc833ff68bcd0ec213aeaf5f5187ee1
dinhnv <vdbkpro@gmail.com>
2020-07-14 12:06:55 +0700

    cros domain

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
  */
 
 use Library\Route;
-
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header ("Access-Control-Allow-Headers: *");
+header ("Access-Control-Max-Age: 86400");
 define("DIR", dirname(__FILE__));
 require 'Config/Init.php';
 $uri=isset($_GET['uri'])?$_GET['uri']:'';
