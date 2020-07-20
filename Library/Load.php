7117ed7f9aabba3830a8100d99eec0890a66575d
vdbkpro <vdbkpro@gmail.com>
2020-07-20 09:21:47 +0700

    autoload minimum

--- Library/Load.php
+++ Library/Load.php
@@ -1,27 +1,44 @@
 <?php
 namespace Library;
 class Load{
+    public static $fileMap = [];
     public static function autoLoad(){
-        require_once DIR.'/Controller/Controller.php';
-        require_once DIR.'/Model/Model.php';
-        require_once DIR.'/Model/SqlObject.php';
-        self::requireDir('/Library/');
-        self::requireDir('/Config/');
-        self::requireDir('/Model/');
-        self::requireDir('/Controller/');
+        require_once DIR . '/vendor/autoload.php';
+        self::requireDir('/Controller');
+        self::requireDir('/Model');
+        self::requireDir('/Library');
+        spl_autoload_register(function ($class_name) {
+            $classPath = explode('\\',$class_name);
+            $class_name = end($classPath).".php";
+            if(isset(self::$fileMap[$class_name])){
+                foreach(self::$fileMap[$class_name] as $path){
+                    require_once $path;
+                }
+            }
+        });
+        self::requireDir('/Config',true);
     }
-    public static function requireDir($dirname){
+    public static function requireDir($dirname,$require=false){
         $ListFile=scandir(DIR.$dirname);
         foreach ($ListFile as $fileName) {
             if($fileName != '..' && $fileName != '.'){
                 $path = DIR.$dirname.'/' . $fileName;
-                if (is_file($path)&&stripos($path,'.php')!==false) {
-                    require_once $path;
+                if (is_file($path)) {
+                    self::addFileToFileMap($path,$fileName);
+                    if($require){
+                        require_once $path;
+                    }
                 }
                 else if(is_dir($path)){
-                    self::requireDir($dirname.'/' . $fileName);
+                    self::requireDir($dirname.'/' . $fileName,$require);
                 }
             }
         }
+    }
+    private static function addFileToFileMap($path,$fileName){
+        if(!isset(self::$fileMap[$fileName])){
+            self::$fileMap[$fileName] = [];
+        }
+        self::$fileMap[$fileName][] = $path;
     }
 }   
