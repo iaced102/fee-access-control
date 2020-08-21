3711cdd994ed4e982a2da1504179e9f8e1e89470
dinhnv <vdbkpro@gmail.com>
2020-08-21 17:26:56 +0700

    get all

--- Library/AccessControl.php
+++ Library/AccessControl.php
@@ -38,7 +38,7 @@ class AccessControl{
     */
     public static function checkRoleActionRemote($roleIdentifier,$objectIdentifier,$action){
         $key = json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier,'action'=>$action]);
-        if(!self::checkMemcache($roleIdentifier,$objectIdentifier)){
+        if(self::checkMemcache($roleIdentifier,$objectIdentifier)!==false){
             self::getRoleActionRemote($roleIdentifier,$objectIdentifier);    
         }
         return CacheService::get($key);
@@ -48,23 +48,59 @@ class AccessControl{
     * Lấy quyền về memcache, và set flag là đã lấy bất kể là có quyền hoặc không.
     */
     public static function getRoleActionRemote($roleIdentifier,$objectIdentifier){
+        $listAction = []; 
         $dataResponse = Request::request(Request::API_ACCESS_CONTROL."/roles/$roleIdentifier/accesscontrol/$objectIdentifier");
         if(is_array($dataResponse)&&isset($dataResponse['status']) && $dataResponse['status']==STATUS_OK && isset($dataResponse['data'])){
             if(is_array($dataResponse['data']) && count($dataResponse['data'])>0){
                 foreach($dataResponse['data'] as $accessControl){
                     $keyAccessControl = json_encode(['role'=>$accessControl['role_identifier'],'object'=>$accessControl['object_identifier'],'action'=>$accessControl['action']]);
                     CacheService::set($keyAccessControl,$accessControl['status']);
+                    $listAction[]=$accessControl['action'];
                 }
             }
         }
+        CacheService::set(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier]),$listAction);
+    }
+     /*
+    * Lấy quyền về memcache all 
+    */
+    public static function getRoleActionRemoteAllObject($roleIdentifier){
+        $listAction = []; 
+        $dataResponse = Request::request(Request::API_ACCESS_CONTROL."/roles/$roleIdentifier/accesscontrol");
+        if(is_array($dataResponse)&&isset($dataResponse['status']) && $dataResponse['status']==STATUS_OK && isset($dataResponse['data'])){
+            if(is_array($dataResponse['data']) && count($dataResponse['data'])>0){
+                foreach($dataResponse['data'] as $accessControl){
+                    $keyAccessControl = json_encode(['role'=>$accessControl['role_identifier'],'object'=>$accessControl['object_identifier'],'action'=>$accessControl['action']]);
+                    CacheService::set($keyAccessControl,$accessControl['status']);
+                    if(!isset($listAction[$accessControl['object_identifier']])){
+                        $listAction[$accessControl['object_identifier']]=[];
+                    }
+                    $listAction[$accessControl['object_identifier']][]=$accessControl['action'];
+                }
+            }
+        }
+        foreach($listAction as $object=>$actionsByObject){
+            CacheService::set(json_encode(['role'=>$roleIdentifier,'object'=>$object]),$actionsByObject);
+        }
         
-        CacheService::set(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier,'flag'=>true]),true);
     }
     /*
     *check xem đã được sync về chưa. Khác với đã lấy về nhưng không có quyền
     */
     public static function checkMemcache($roleIdentifier,$objectIdentifier){
-        return CacheService::get(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier,'flag'=>true]));
+        return CacheService::get(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier]));
+    }
+    /*
+    * get List Action by Object Identifier
+    */
+    public static function getListAction($objectIdentifier){
+        $roleIdentifier = Auth::getCurrentRole();
+        $listActionInMemcache = CacheService::get(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier]));
+        if($listActionInMemcache===false){
+            self::getRoleActionRemote($roleIdentifier,$objectIdentifier);    
+            return CacheService::get(json_encode(['role'=>$roleIdentifier,'object'=>$objectIdentifier]));//self::getListAction($roleIdentifier,$objectIdentifier);
+        }
+        return $listActionInMemcache;
     }
     
     public static function filterByPermission($objectIdentifier,$action){
