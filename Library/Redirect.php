abbd5c58d40d97d59369e2ec788e94ba84784a60
vdbkpro <vdbkpro@gmail.com>
2020-11-21 14:13:58 +0700

    update router cho phéo filter role BA

--- Library/Redirect.php
+++ Library/Redirect.php
@@ -10,6 +10,16 @@ class Redirect{
         self::Redirect('/404.html');
         exit;
     }
+    public static function redirect403(){
+        header('Content-Type: application/json');
+        $output = [
+            'status'=>STATUS_PERMISSION_DENIED,
+            'message'=> Message::getStatusResponse(STATUS_PERMISSION_DENIED)
+        ];
+        print json_encode($output);
+        
+        exit;
+    }
     public static function redirectByJavascript($url){
         echo "<script>window.location='".$url."'</script>";
     }
