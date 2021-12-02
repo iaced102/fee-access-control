f1c7b5708e1538ca750ad72793af9721c5b3e94f
khadmSymper <khadm@symper.vn>
2021-12-02 16:07:14 +0700

    ITD2108032 Fix lỗi sql injection khi dùng getByFilter

--- Model/ModelFilterHelper.php
+++ Model/ModelFilterHelper.php
@@ -165,7 +165,6 @@ class ModelFilterHelper
 
         return $sort;
     }
-
     private static function getFrom($table)
     {
         if (stripos($table, "select ") !== false) {
@@ -212,6 +211,7 @@ class ModelFilterHelper
         // get search query
         $searchKey = $filter['search'];
         if (trim($searchKey) != '') {
+            $searchKey = pg_escape_string($searchKey);
             $searchConditions = [];
             foreach ($columns as $colName) {
                 $searchConditions[] = " CAST(\"$colName\" AS VARCHAR) ILIKE '%$searchKey%' ";
@@ -270,6 +270,9 @@ class ModelFilterHelper
         if (array_key_exists('valueFilter', $conditionItem)) {
             $colType = $mapColumns[$colName];
             $values = '';
+            foreach ($conditionItem['valueFilter']['values'] as $key => $vl) {
+                $conditionItem['valueFilter']['values'][$key] = pg_escape_string($vl);
+            }
             if ($colType == 'number') {
                 $values = implode(' , ', $conditionItem['valueFilter']['values']);
                 $values = "($values)";
@@ -293,6 +296,11 @@ class ModelFilterHelper
             $colDef = $mapColumns[$colName];
             if ($op == 'in' || $op == 'not_in') {
                 $colType = $colDef['type'];
+                if(is_array($value)){
+                    foreach ($value as $key => $vl) {
+                        $value[$key] = pg_escape_string($vl);
+                    }
+                }
                 if ($colType == 'number') {
                     $value = implode(' , ', $value);
                     $value = "($value)";
@@ -324,6 +332,7 @@ class ModelFilterHelper
         if (array_key_exists($colName, $mapColumns)) {
             $colDef = $mapColumns[$colName];
             if (!array_key_exists($op, self::$notCheckType)) {
+                $value = pg_escape_string($vl);
                 if ($colDef['type'] != 'number') {
                     $value = "'$value'";
                 }
