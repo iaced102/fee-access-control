804355446d6e1470f63de315daa3218f3fcd7ff2
Dinh Hoang <hoangbvh98@gmail.com>
2022-02-24 11:42:40 +0700

    sửa index theo ngĩn

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+ARG REPO_LOCATION=registry.symper.vn
+ARG BASE_VERSION=1.0
+FROM ${REPO_LOCATION}/php-7.4.8-master:${BASE_VERSION}
+# Copy source code
+WORKDIR /src
+ADD ./src /var/www/accesscontrol.symper.vn
+EXPOSE 9000
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
  * Date: 9/3/2015
  * Time: 3:42 PM
  */
-
 use Library\Route;
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
@@ -15,8 +14,11 @@ header("Access-Control-Max-Age: 86400");
 define("DIR", dirname(__FILE__));
 if(file_exists('env.json')){
     $GLOBALS['env'] = json_decode(file_get_contents('env.json'),true);
+    $uri = $_SERVER['REQUEST_URI'];
+    $uri = ltrim($uri, '/');
+    $uriArr = explode("?",$uri);
+    $uri = $uriArr[0];
     require 'Config/Init.php';
-    $uri=isset($_GET['uri'])?$_GET['uri']:'';
     if($uri!='404.html'){
         Route::performRequest($uri);
     }
