95264bef6efe89f1aa8ef11857669b8c5e2d2b21
klien0909 <ttklien9900@gmail.com>
2022-10-27 11:05:21 +0700

    update

--- Library/Auth.php
+++ Library/Auth.php
@@ -192,6 +192,15 @@ class Auth
         }
         return false;
     }
+    public static function getCurrentBaEmail(){
+        $token = Auth::getBearerToken();
+        if(!empty($token)){
+            $dataLogin = Auth::getJwtData($token);
+            $baEmail = (!empty($dataLogin['email'])) ? $dataLogin['email'] : "";
+            return $baEmail;
+        }
+        return "";
+    }
     public static function getCurrentUserId()
     {
         $dataLogin = self::getDataToken();
