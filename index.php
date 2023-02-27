1cd877d4c3019e51c3d680c36525f3be68879f6b
Dinh Hoang <hoangbvh98@gmail.com>
2023-02-27 15:10:54 +0700

    update

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 
 use Library\Route;
 
-var_dump("ok");
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
