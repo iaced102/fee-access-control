343585967de4d6f2d311e543c594e69495f1c7b8
khadm <khadm@symper.vn>
2023-03-02 14:34:01 +0700

    Thêm hàm lưu các object vào access control khi migrate data

--- Controller/ObjectTenantMigration.php
+++ Controller/ObjectTenantMigration.php
@@ -1,7 +1,9 @@
 <?php
 namespace Controller;
 
+use Library\Auth;
 use Library\Message;
+use Library\Request;
 use Model\Users;
 
 class ObjectTenantMigration extends Controller{
@@ -10,6 +12,40 @@ class ObjectTenantMigration extends Controller{
         parent::__construct();
         $this->defaultAction = 'migrate';
         $this->requireLogin = true;
+    }
+
+    /**
+     * @param array $listObj mảng của các object, có dạng:
+     * [
+     *      [
+     *          'id' => '',
+     *          'name' => '', // optional
+     *          'title' => '',
+     *          'objectType' => '', // optional
+     *      ]
+     * ]
+     */
+    public function saveObjectIdentify($objType,$listObj,$target){
+        $arr = [];
+        if(count($listObj)>0){
+            foreach($listObj as $k=>$v){
+                $obj = ['objectIdentifier'=>$objType.':'.$v->id,
+                    'title'=>$v->title,
+                    'name'=>$objType=='document_definition'? $v->name:'',
+                    'type'=>$objType,
+                    'objectType'=>isset($v->objectType)?$v->objectType:'',
+                    'tenantId'=>$target
+                ];
+                array_push($arr,$obj);
+            }
+            $dataPost = [
+                'listObj'=>$arr
+            ];
+            $token = "Bearer ".Auth::getBearerToken();
+            $response =Request::request(ACCESS_CONTROL_SERVICE.'/object-identify',$dataPost,'POST',$token, 'application/json', false);
+            $response = json_decode($response);
+            return $response;
+        }
     }
 
     /**
